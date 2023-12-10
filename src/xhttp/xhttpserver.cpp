eb91927c0cfa638edd8c3c3aac318b75b12088a6
JxxJxx11 <1548274212@qq.com>
2023-12-10 21:34:08 +0800

    modified

--- src/xhttp/xhttpserver.cpp
+++ src/xhttp/xhttpserver.cpp
@@ -27,7 +27,7 @@ public:
 
 			printf("========recv========\n%s====================\n", buf);
 			string src = buf;
-			string pattern = "^([A-Z]+) (.+) HTTP/1";
+			string pattern = "^([A-Z]+) /([a-zA-Z0-9]*([.][a-zA-Z0-9]*)?)[?]?(.*)? HTTP/1";
 			regex r(pattern);
 			smatch mas;
 			regex_search(src, mas, r);
@@ -38,8 +38,16 @@ public:
 			}
 
 			string type = mas[1];
-			string path = mas[2];
+			string path = "/";
+			path += mas[2];
+			string filetype = mas[3];
+			string query = mas[4];
+			if (filetype.size() > 0) {
+				filetype = filetype.substr(1, filetype.size() - 1);
+			}
+			printf("type: [%s]\n path:[%s]\n filetype:[%s]\n query:[%s]\n", type.c_str(), path.c_str(), filetype.c_str(),query.c_str());
 			if (type != "GET") {
+				printf("not GET!\n");
 				Close();
 				return;
 			}
@@ -49,8 +57,29 @@ public:
 			}
 			string filepath = "www";
 			filepath += filename;
+
+			if (filetype == "php") {
+				string cmd = "php-cgi ";
+				cmd += filepath;
+				cmd += " ";
+				for (int i = 0; i < query.size(); i++) {
+					if (query[i] == '&') query[i] = ' ';
+				}
+				cmd += query;
+				cmd += " > ";
+				filepath += ".html";
+				cmd += filepath;
+
+				
+
+				printf("%s\n", cmd.c_str());
+				system(cmd.c_str());
+			}
+
+
 			FILE* fp = fopen(filepath.c_str(), "rb");
 			if (fp == NULL) {
+				printf("open file %s failed!\n", filepath.c_str());
 				Close();
 				return;
 			}
@@ -58,6 +87,20 @@ public:
 			int filesize = ftell(fp);
 			fseek(fp, 0, SEEK_SET);
 			printf("file size is %d\n", filesize);
+			if (filetype == "php") {
+				char c = 0;
+				int headsize = 0;
+				while (fread(&c, 1, 1, fp) > 0) {
+					headsize++;
+					if (c == '\r') {
+						fseek(fp, 3, SEEK_CUR);
+						headsize += 3;
+						break;
+					}
+				}
+				filesize = filesize - headsize;
+			}
+			
 
 			string rmsg = "";
 			rmsg = "HTTP/1.1 200 OK\r\n";
@@ -82,6 +125,7 @@ public:
 				int re = client.Send(buf, len);
 				if (re <= 0) break;
 			}
+			printf("========end========\n\n\n");
 		}
 		
 		Close();
